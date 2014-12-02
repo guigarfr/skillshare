347daaff8cfa7c4217a74dc34608100f1fdeb71a
Guillermo García Franco <guigarfr@macbook-de-guillermo-garcia-franco.local>
2014-12-02 16:47:46 +0100

    Initial commit

---
+++ mvp_landing/email_gmail_info.py
@@ -0,0 +1,7 @@
+
+#for gmail or google apps
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'guigarfr+skillshare@gmail.com'
+EMAIL_HOST_PASSWORD = 'yourpassord'
+EMAIL_PORT = 587
